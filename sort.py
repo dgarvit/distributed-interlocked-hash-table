3a1afce2f458ca75e91c77cf90afb9814185f84b
Garvit Dewan <d.garvit@gmail.com>
2019-09-28 16:03:13 +0530

    Add average stats

--- sort.py
+++ sort.py
@@ -13,15 +13,31 @@ times.sort(key=lambda tup: (tup[1], tup[0]), reverse=True)
 maxStr = "Max " + str(times[0][1]) + ": " + str(times[0][0])
 print maxStr
 
+b = times[0][0]
+count = 1
+
 for i in xrange(0, len(times)-1):
     if str(times[i][1]) != str(times[i+1][1]):
         minStr = "Min " + str(times[i][1]) + ": " + str(times[i][0])
         maxStr = "Max " + str(times[i+1][1]) + ": " + str(times[i+1][0])
+        avgStr = "Avg " + str(times[i+1][1]) + ": " + str(b/count)
         print minStr
+        print avgStr
+        print ""
         print maxStr
+        count = 0
+        b = 0.0
+    else:
+        b += times[i][0]
+        count += 1
+
+b += times[-1][0]
+count += 1
 
 minStr = "Min " + str(times[-1][1]) + ": " + str(times[-1][0])
+avgStr = "Avg " + str(times[-1][1]) + ": " + str(b/count)
 print minStr
+print avgStr
 
 with open("sortByFunctions.txt", 'w') as f:
     for a in times:
